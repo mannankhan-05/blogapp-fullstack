54cabe81729159eb4878432c0052a60024016863
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-05-13 14:25:57 -0400

    added the blogs from the database

--- frontend/src/components/mainPage.vue
+++ frontend/src/components/mainPage.vue
@@ -1,14 +1,22 @@
 <template>
-  <v-layout>
+  <!-- <v-layout> -->
+  <div>
     <v-card
       variant="tonal"
       class="pa-2 ma-3"
       width="400"
       v-for="blogs in AllBlogs"
       :key="blogs.b_id"
-      >{{ blogs.b_id }}</v-card
     >
-  </v-layout>
+      <h6 class="pa-0 mb-3">{{ blogs.b_id }}</h6>
+      <h2 class="text-decoration-underline mb-3">{{ blogs.b_title }}</h2>
+      <p class="mb-3">{{ blogs.b_description }}</p>
+      <h5 class="mb-0 bg-grey text-white text-center">
+        Author : {{ blogs.b_author }}
+      </h5>
+    </v-card>
+  </div>
+  <!-- </v-layout> -->
 </template>
 
 <script>
@@ -22,7 +30,9 @@ export default {
   },
   async mounted() {
     let response = await axios.get("http://localhost:3000/allBlogs");
-    this.AllBlogs.push(response.data);
+    response.data.forEach((blog) => {
+      this.AllBlogs.push(blog);
+    });
   },
 };
 </script>
