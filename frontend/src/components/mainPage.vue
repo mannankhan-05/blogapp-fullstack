1016b9fcfdea193360243838eebb48b253d34802
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-05-14 06:29:54 -0400

    added the images in the blogs

--- frontend/src/components/mainPage.vue
+++ frontend/src/components/mainPage.vue
@@ -1,22 +1,30 @@
 <template>
-  <!-- <v-layout> -->
-  <div>
-    <v-card
-      variant="tonal"
-      class="pa-2 ma-3"
-      width="400"
-      v-for="blogs in AllBlogs"
-      :key="blogs.b_id"
-    >
-      <h6 class="pa-0 mb-3">{{ blogs.b_id }}</h6>
-      <h2 class="text-decoration-underline mb-3">{{ blogs.b_title }}</h2>
-      <p class="mb-3">{{ blogs.b_description }}</p>
-      <h5 class="mb-0 bg-grey text-white text-center">
-        Author : {{ blogs.b_author }}
-      </h5>
-    </v-card>
-  </div>
-  <!-- </v-layout> -->
+  <v-layout>
+    <v-flex>
+      <div>
+        <v-card
+          variant="tonal"
+          class="pa-2 ma-3"
+          width="400"
+          v-for="blogs in AllBlogs"
+          :key="blogs.b_id"
+        >
+          <h6 class="pa-0 mb-3">{{ blogs.b_id }}</h6>
+          <!-- <v-btn v-if="editBlog">edit</v-btn> -->
+          <h2 class="text-decoration-underline mb-3">{{ blogs.b_title }}</h2>
+          <img
+            :src="require(`@/assets/${blogs.b_picture}`)"
+            class="mb-3 image"
+          />
+
+          <p class="mb-3">{{ blogs.b_description }}</p>
+          <h5 class="mb-0 bg-grey text-white text-center">
+            Author : {{ blogs.b_author }}
+          </h5>
+        </v-card>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -26,6 +34,7 @@ export default {
   data() {
     return {
       AllBlogs: [],
+      // editBlog: false,
     };
   },
   async mounted() {
@@ -37,4 +46,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.image {
+  height: 5%;
+  width: 100%;
+  border-radius: 5px;
+}
+</style>
