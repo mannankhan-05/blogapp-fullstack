1503c2c9ca127a516cc5891750880d064e964868
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-06-06 13:53:27 -0400

    added the carousel in the homePage of the blogApp

--- frontend/src/components/homePage.vue
+++ frontend/src/components/homePage.vue
@@ -1,6 +1,19 @@
 <template>
   <v-container>
     <div class="ma-3">
+      <v-carousel class="rounded border-sm">
+        <v-carousel-item
+          src="https://images.unsplash.com/opengraph/1x1.png?blend=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1486312338219-ce68d2c6f44d%3Fblend%3D000000%26blend-alpha%3D10%26blend-mode%3Dnormal%26crop%3Dfaces%252Cedges%26h%3D630%26mark%3Dhttps%253A%252F%252Fimages.unsplash.com%252Fopengraph%252Fsearch-input.png%253Fh%253D84%2526txt%253Dblog%2526txt-align%253Dmiddle%25252Cleft%2526txt-clip%253Dellipsis%2526txt-color%253D000000%2526txt-pad%253D80%2526txt-size%253D40%2526txt-width%253D660%2526w%253D750%2526auto%253Dformat%2526fit%253Dcrop%2526q%253D60%26mark-align%3Dmiddle%252Ccenter%26mark-w%3D750%26w%3D1200%26auto%3Dformat%26fit%3Dcrop%26q%3D60%26ixid%3DM3wxMjA3fDB8MXxzZWFyY2h8M3x8YmxvZ3xlbnwwfHx8fDE3MTc1NjQzODh8MA%26ixlib%3Drb-4.0.3&blend-w=1&h=630&mark=https%3A%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&mark-align=top%2Cleft&mark-pad=50&mark-w=64&w=1200&auto=format&fit=crop&q=60"
+        ></v-carousel-item>
+        <v-carousel-item
+          src="https://cameroondeskacademy.com/wp-content/uploads/2018/12/blogging-SMB.png.webp"
+        ></v-carousel-item>
+
+        <v-carousel-item
+          src="https://www.ascian.in/creatives/wp-content/uploads/blog.jpg"
+        ></v-carousel-item>
+      </v-carousel>
+
       <h2 class="mt-5 mb-0 text-decoration-underline font-weight-black">
         Sample Blogs
       </h2>
